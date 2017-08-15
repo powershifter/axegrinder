878a5a4c7b4eb0f36d9e7828684dcedd36f38fd6
Cory LaFlamme <me@corylaflamme.com>
2017-08-15 18:05:13 -0400

    Started switching to nightmare from selenium.

--- crawler.js
+++ crawler.js
@@ -5,6 +5,7 @@ const WebDriver = require('selenium-webdriver');
 const Phantom = require('phantomjs-prebuilt');
 const Crawler = require('simplecrawler');
 const chalk = require('chalk');
+const async = require('async');
 
 const csv = require('./csv');
 const download = require('./driver')();
@@ -14,16 +15,38 @@ module.exports = (argv) => {
   const crawler = Crawler(argv.url);
   const writer = csv(argv.csv);
   const levels = argv.levels.split(',');
+  const urlList = [];
 
   crawler.on('crawlstart', () => {
-    console.log('\nBuilding initial map. One moment, please...\n');
+    console.log('\n--- Building sitemap. One moment, please...\n');
   });
 
-  crawler.on('fetchstart', function(queueItem, requestOptions) {
-    download(queueItem.url, levels, (err, results) => {
-      if (argv.csv && results.violations.length > 0) {
-        writer.addViolations(queueItem.url, results.violations);
-      }
+  crawler.on('queueadd', (queueItem) => {
+    urlList.push(queueItem.url);
+    console.log(`Added item #${ queueItem.id } to queue: ${ queueItem.uriPath }`);
+  });
+
+  // crawler.on('fetchcomplete', function(queueItem, requestOptions) {
+  //   const next = this.wait();
+  //   download(queueItem.url, levels, (err, results) => {
+  //     if (argv.csv && results.violations.length > 0) {
+  //       writer.addViolations(queueItem.url, results.violations);
+  //     }
+  //     next();
+  //   });
+  // });
+
+  crawler.on('complete', () => {
+    console.log('\n--- Map completed! Running tests...\n');
+    async.eachSeries(urlList, (url, callback) => {
+      download(url, levels, (err, results) => {
+        console.log('TEST ONNNNEEEEE')
+
+        if (argv.csv && results.violations.length > 0) {
+          writer.addViolations(url, results.violations);
+        }
+        callback();
+      });
     });
   });
 
--- driver.js
+++ driver.js
@@ -2,6 +2,8 @@ const AxeBuilder = require('axe-webdriverjs');
 const WebDriver = require('selenium-webdriver');
 const Phantom = require('phantomjs-prebuilt');
 const chalk = require('chalk');
+const Nightmare = require('nightmare');
+const axe = require('axe-core');
 
 function logResults(url, results) {
   if (results.violations.length > 0) {
@@ -17,24 +19,42 @@ module.exports = () => {
 
   // Custom selenium driver that uses the bundled phantomjs, so we don't need a
   // binary in the PATH.
-  const customPhantom = WebDriver.Capabilities.phantomjs();
-  customPhantom.set('phantomjs.binary.path', Phantom.path);
-  const driverTemplate = new WebDriver.Builder().withCapabilities(customPhantom);
+  // const customPhantom = WebDriver.Capabilities.phantomjs();
+  // customPhantom.set('phantomjs.binary.path', Phantom.path);
+  // const driverTemplate = new WebDriver.Builder().withCapabilities(customPhantom);
+
+  const nightmare = Nightmare();
+  nightmare.inject('js', 'node_modules/axe-core/axe.js');
 
   return (url, levels, callback) => {
-    const driver = driverTemplate.build();
-    driver.get(url).then(() => {
-      try {
-        AxeBuilder(driver).withTags(levels).analyze((results) => {
-          driver.quit();
-          logResults(url, results);
-          callback(null, results);
-        });
-      } catch(e) {
-        console.error(chalk.red('Error fetching ' + url));
-        callback(e, null);
-      }
+    nightmare.goto(url).wait('body').evaluate((onAxeRun) => {
+      axe.run(function(err, results) {
+        onAxeRun(results);
+      });
+    }).then((results) => {
+      console.log('success!!!!!!!!')
+      logResults(url, results);
+      callback(null, results);
+    }).catch((err) => {
+      console.error(chalk.red('Error fetching ' + url));
+      callback(e, null);
     });
   };
+
+  // return (url, levels, callback) => {
+  //   const driver = driverTemplate.build();
+  //   driver.get(url).then(() => {
+  //     try {
+  //       AxeBuilder(driver).withTags(levels).analyze((results) => {
+  //         driver.quit();
+  //         logResults(url, results);
+  //         callback(null, results);
+  //       });
+  //     } catch(e) {
+  //       console.error(chalk.red('Error fetching ' + url));
+  //       callback(e, null);
+  //     }
+  //   });
+  // };
 
 };
